8cf7f9d512933796abe78e3ffe56b842b7d48a6e
Davide64-dev <davidpcu@hotmail.com>
2024-05-24 17:17:07 +0100

    small change

--- src/main/pt/up/fe/comp2024/analysis/AnalysisVisitor.java
+++ src/main/pt/up/fe/comp2024/analysis/AnalysisVisitor.java
@@ -127,7 +127,7 @@ public abstract class AnalysisVisitor extends PreorderJmmVisitor<SymbolTable, Vo
             return new Type("int", false);
         }
 
-        if (variable.getKind().equals(Kind.NEW_ARRAY) || variable.getKind().equals(Kind.ARRAY_CALL)){
+        if (variable.getKind().equals(Kind.NEW_ARRAY.toString()) || variable.getKind().equals(Kind.ARRAY_CALL.toString())){
             return new Type("int", true);
         }
 
